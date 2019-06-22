69ddd6eff8dbfe4bbd13dd8ca03a883320ae69fb
Sungmin Kim <seongmin.kim082@gmail.com>
2019-06-22 14:29:53 -0400

    Added style on a tag

--- client/src/components/InitPage.js
+++ client/src/components/InitPage.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import style from "../assets/styles/partial/initPage.module.scss";
 
 class InitPage extends Component {
@@ -8,12 +8,22 @@ class InitPage extends Component {
       <div className={style.container_initPage}>
         <ul>
           <li>
-            <Link to="ozen_sushi">Ozen Sushi</Link>
+            <NavLink
+              className={style.container_initPage__ul__li__a}
+              to="ozen_sushi"
+            >
+              Ozen Sushi
+            </NavLink>
           </li>
         </ul>
         <ul>
           <li>
-            <Link to="ozen_korea">Ozen Korea</Link>
+            <NavLink
+              className={style.container_initPage__ul__li__a}
+              to="ozen_korea"
+            >
+              Ozen Korea
+            </NavLink>
           </li>
         </ul>
       </div>
