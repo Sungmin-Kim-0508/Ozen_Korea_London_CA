de3b60783f70f2a2651965b195bb1662f539018e
Sungmin Kim <seongmin.kim082@gmail.com>
2019-06-22 14:07:54 -0400

    Add initPage.scss

--- client/src/components/InitPage.js
+++ client/src/components/InitPage.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import style from "../assets/styles/partial/initPage.module.scss";
 
 class InitPage extends Component {
   render() {
     return (
-      <div>
+      <div className={style.container_initPage}>
         <ul>
           <li>
             <Link to="ozen_sushi">Ozen Sushi</Link>
