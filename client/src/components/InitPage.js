96208e56a1732a9af0538f531b03fe3327138f9f
TongilKim <kkimtt1234@gmail.com>
2019-07-06 16:24:18 -0400

    making initpage

--- client/src/components/InitPage.js
+++ client/src/components/InitPage.js
@@ -1,32 +1,28 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import { NavLink } from "react-router-dom";
 import style from "../assets/styles/partial/initPage.module.scss";
+import 'bootstrap/dist/css/bootstrap.css';
 
 class InitPage extends Component {
   render() {
     return (
-      <div className={style.container_initPage}>
-        <ul>
-          <li>
-            <NavLink
-              className={style.container_initPage__ul__li__a}
-              to="ozen_sushi"
-            >
+      <Fragment>
+        <div className={style.container_initPage}>
+
+          <div>
+            <div className={style.leftside}>
               Ozen Sushi
-            </NavLink>
-          </li>
-        </ul>
-        <ul>
-          <li>
-            <NavLink
-              className={style.container_initPage__ul__li__a}
-              to="ozen_korea"
-            >
+            </div>
+          </div>
+
+          <div>
+            <div className={style.rightside}>
               Ozen Korea
-            </NavLink>
-          </li>
-        </ul>
-      </div>
+            </div>
+          </div>
+
+        </div>
+      </Fragment>
     );
   }
 }
